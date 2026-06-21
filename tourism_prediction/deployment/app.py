aa81482a5affa76e11fef7c3b15430e35b519edb
mshahban <mshahban.ahmed@gmail.com>
2026-06-21 23:50:21 +0530

    Update app.py

--- tourism_prediction/deployment/app.py
+++ tourism_prediction/deployment/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from huggingface_hub import hf_hub_download
 
 st.title("Tourism Prediction")
-st.success("Application Started Successfully")
+
+file_path = hf_hub_download(
+    repo_id="mshahban/tourism-package-model",
+    filename="best_tourism_package_model_v1.joblib"
+)
+
+st.success("Model downloaded successfully")
+st.write(file_path)
